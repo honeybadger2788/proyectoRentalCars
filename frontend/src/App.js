74702d5c23f293422897cd02cad093b35dca9294
Andrea Lin <andre93.lin@gmail.com>
2022-10-20 07:20:26 +0800

    add material ui, configure theme color. remove tailwind.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,30 @@
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
-import theme from './theme';
+import Button from '@mui/material/Button';
 
-import { Typography } from './components/Typography';
-import { Title } from './styles';
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1DBEB4',
+      light: '#50dad1',
+      dark: '#29938c',
+      contrastText: '#fff',
+    },
+    secondary: {
+      main: '#383B58',
+      light: '#545776',
+      dark: '#262838',
+      contrastText: '#000',
+    },
+  },
+});
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <h1 className="text-5xl font-bold underline text-indigo-600">
-        Hello world!!!
-      </h1>
-
-      <Typography variant="h1" class="text-yellow-500">
-        h1
-      </Typography>
-      <Typography variant="h2">h2</Typography>
-      <Typography variant="h3">h3</Typography>
-      <Typography variant="h4">h4</Typography>
-
-      <button>
-        <Typography variant="button1">Button 1</Typography>
-      </button>
-      <button>
-        <Typography variant="button2">Button 2</Typography>
-      </button>
-
-      <Typography variant="body1">Text 1</Typography>
-      <Typography variant="body2">Text 2</Typography>
-
-      <Title>Styled title</Title>
+      <CssBaseline />
+      <Button variant="contained">MUI button</Button>
     </ThemeProvider>
   );
 }
