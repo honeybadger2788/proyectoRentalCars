b32a6d718218048806990e5bbad043d57d74d69e
Noelia Carosella <noe.carosella@gmail.com>
2022-10-20 22:48:47 +0000

    Css theme

---
+++ backend/src/main/java/final_project_group_2/WebApplication/WebApplication.java
@@ -0,0 +1,13 @@
+package final_project_group_2.WebApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+
+}
