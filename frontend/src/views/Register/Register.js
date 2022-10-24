3ff02fc69aab9e55ea27d71249aff8ea799e9804
Noelia Carosella <noe.carosella@gmail.com>
2022-10-24 15:22:03 +0000

    Fix 9 login and register template

--- frontend/src/views/Register/Register.js
+++ frontend/src/views/Register/Register.js
@@ -1,40 +1,41 @@
 import React from "react";
+import styles from './Register.module.css'
 
-export default function Register(props) {
+export default function Register() {
     return (
         <div>
-            <h1 className='login-title'>Crear cuenta</h1>
+            <h1 className={styles.title}>Crear cuenta</h1>
             <form>
-                <div className='name-container'>
-                    <div className='input-container'>
+                <div className={styles.nameContainer}>
+                    <div className={styles.container}>
                         <label for='firstName'>Nombre</label>
-                        <input className='name'id='firstName'type='text'/>
+                        <input className={styles.name}id='firstName'type='text'/>
                     </div>
                 
-                    <div className='input-container'>
+                    <div className={styles.container}>
                         <label for='lastName'>Apellido</label>
-                        <input className='name'id='lastName'type='text'/>
+                        <input className={styles.name}id='lastName'type='text'/>
                     </div>
                 </div>
                 
-                <div className='input-container'>
+                <div className={styles.container}>
                     <label for='email'>Correo electrónico</label>
                     <input id='email'type='email'/>
                 </div>
 
-                <div className='input-container'>
+                <div className={styles.container}>
                     <label for='password'>Contraseña</label>
                     <input id='password'type='password'/>
                 </div>
                 
-                <div className='input-container'>
+                <div className={styles.container}>
                     <label for='confirmPassword'>Confirmar contraseña</label>
                     <input id='confirmPassword'type='password'/>
                 </div>
                 
                 <button type='submit'>Crear cuenta</button>
             </form>
-            <p className='small-text text-center'>¿Ya tiene una cuenta? <a href='/'>Iniciar sesión</a></p>
+            <p className='small-text center'>¿Ya tiene una cuenta? <a href='/'>Iniciar sesión</a></p>
         </div>
     )
   }
