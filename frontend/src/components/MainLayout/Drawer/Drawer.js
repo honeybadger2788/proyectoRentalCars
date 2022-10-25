7b232c54cdf92f943412d41b323ea807016da77e
honeybadger2788 <noe.carosella@gmail.com>
2022-10-25 11:02:42 -0300

    add login event

--- frontend/src/components/MainLayout/Drawer/Drawer.js
+++ frontend/src/components/MainLayout/Drawer/Drawer.js
@@ -1,10 +1,13 @@
+import { useState, useEffect } from 'react'
 import { Link, useLocation } from 'react-router-dom';
 
 import Social from '../Social/Social';
-
+import User from '../User/User';
 import styles from './Drawer.module.css';
 
 function Drawer({ toogleDrawer }) {
+  const [user, setUser] = useState({})
+  const [userLoggedIn, setUserLoggedIn] = useState(false)
   const location = useLocation();
 
   const onClickLink = () => {
@@ -15,36 +18,46 @@ function Drawer({ toogleDrawer }) {
     toogleDrawer();
   };
 
+  const onClickLogout = () => {
+    sessionStorage.clear()
+    onClose()
+  }
+
+  useEffect(() => {
+    if (sessionStorage.user && sessionStorage.userLoggedIn) {
+      setUser(JSON.parse(sessionStorage.user))
+      setUserLoggedIn(JSON.parse(sessionStorage.userLoggedIn))
+    }
+  },[])
+
   return (
     <nav className={styles.drawer}>
       <section className={styles.drawer_header}>
         <p className={styles.close} onClick={onClose}>
           x
         </p>
-        <h2>MENÚ</h2>
+        { !userLoggedIn ? <h2>MENÚ</h2> : <User user={user}/>}
       </section>
       <section className={styles.nav_menu}>
-        {location.pathname !== '/register' && (
+        { !userLoggedIn && location.pathname !== '/register' && (
           <Link to="/register" onClick={onClickLink}>
             <h3>Crear cuenta</h3>
           </Link>
         )}
-
-        {location.pathname !== '/login' && (
+        { !userLoggedIn && location.pathname !== '/login' && (
           <Link to="/login" onClick={onClickLink}>
             <h3>Iniciar sesión</h3>
           </Link>
-        )}
-        {/* To-Do implement User context and logout */}
-        {/* <div className={styles.logout}>
+          )}
+          { userLoggedIn && <div className={styles.logout}>
           <p className="small-text">
             ¿Deseas{' '}
-            <a href="#">
-              <span onClick={onClickLink}>cerrar sesión</span>
+            <a href="/">
+              <span onClick={onClickLogout}>cerrar sesión</span>
             </a>
             ?
           </p>
-        </div> */}
+          </div>}
       </section>
       <section className={styles.social}>
         <Social />
