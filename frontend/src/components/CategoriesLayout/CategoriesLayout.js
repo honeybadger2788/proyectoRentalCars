390f47fd999ae8a3094aab1b3f68e28b964b98ae
honeybadger2788 <noe.carosella@gmail.com>
2022-11-11 20:59:09 -0300

    fix categoriesLayout and pom merge conflicts

--- frontend/src/components/CategoriesLayout/CategoriesLayout.js
+++ frontend/src/components/CategoriesLayout/CategoriesLayout.js
@@ -5,4 +5,22 @@ import Category from "./Category/Category";
 export default function CategoriesLayout({ getCategory, categories }) {
   const onClick = (e) => {
     getCategory(e.target.alt)
+  }
+    return (
+      <section className={styles.section}>
+    <section className={styles.section}>
+      <h1 className={styles.h1}>Buscar por tipo de vehículo</h1>
+      <div className={styles.container}>
+          {
+              categories.map((category) => (
+                <Category key={category.title}
+                  image={category.image}
+                  onClick={onClick}
+                >{category.title}</Category>
+              ))
+          }
+          </div>
+        </section>
+        </section>)
+
 }
