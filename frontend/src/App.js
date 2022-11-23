644aee28c2edf1ef2b45f46ed65d4cef4aa7400d
Ariadna Naya <ariadnanaya2016@gmail.com>
2022-11-23 21:30:03 +0000

    Resolve "Crear Pipeline para CI CD"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,6 @@ import ReservationSuccess from './views/Reservation/ReservationSuccess.js';
 import './styles/style.css';
 
 function App() {
-  console.log("Testing pipeline")
   return (
     <div>
       <Routes>
